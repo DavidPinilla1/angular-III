0c0078daabfd1196f11496f6893ad6d9c5557b83
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-03-27 09:34:07 +0100

    added middleware auth changes

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { User } = require('../models/index');
+const { User, Token } = require('../models/index');
 const env = process.env.NODE_ENV || 'development';
 const {  jwt_secret } = require('../config/config.json')[env];
 const authentication = async (req, res, next) => {
@@ -7,6 +7,17 @@ const authentication = async (req, res, next) => {
         const token = req.headers.authorization; //sacamos el token de los headers
         const payload = jwt.verify(token, jwt_secret); //sacamos el payload del token
         const user = await User.findByPk(payload.id); //buscamos el usuario en la base de datos con el id del payload
+        const tokenFound = await Token.findOne({
+            where:{
+                token:token
+            }
+        })
+        if(!user || !tokenFound){
+            res.status(401).send({
+                message: 'No estas autorizado',
+                error
+            })
+        }
         req.user = user; //aquí seteamos el usuario en el objeto request
         next(); //pasamos a la función controladora o al siguiente middleware
     } catch (error) {
